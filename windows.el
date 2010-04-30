4ffe7cbc5d119b3c77de57c98e4d990963bae691
kolektiv <andrew@xyncro.com>
2010-04-30 12:17:01 +0100

    removed cygwin mount for now

--- windows.el
+++ windows.el
@@ -1,11 +1,12 @@
 ; Set up Cygwin.
-(require 'setup-cygwin)
+; (require 'setup-cygwin)
+; Remove for now.
 
 ; Make sure that Cygwin is at the start of the path, so we always use cygwin
 ; utils if available (like ctags)
-(setenv "PATH" 
-	(concat "C:\\Cygwin\\bin;" 
-		(getenv "PATH")))
+; (setenv "PATH" 
+;	(concat "C:\\Cygwin\\bin;" 
+;		(getenv "PATH")))
 
 ; Set Tramp to use sshx. Don't listen to all those who say plink.
 ; That shit is whack.
