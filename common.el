ba3a49b5e0f32d255b5fb656bf88a162318a9346
kolektiv <andrew@xyncro.com>
2012-04-01 19:45:33 +0100

    reverting indent size for js

--- common.el
+++ common.el
@@ -94,7 +94,6 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
-(setq js2-basic-offset 2)
 
 ; Set up coffee-mode
 (require 'coffee-mode)
