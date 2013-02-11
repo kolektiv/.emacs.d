7b23422465da4b6bfbc2f6d50684382a2d38e1d0
kolektiv <andrew@xyncro.com>
2013-02-11 18:50:29 +0000

    removing old osx.el

--- os/osx.el
+++
@@ -1,7 +0,0 @@
-; Set up Erlang from Homebrew
-
-(setq load-path (cons "/usr/local/Cellar/erlang/R15B02/lib/erlang/lib/tools-2.6.8/emacs" load-path))
-(setq erlang-root-dir "/usr/local/Cellar/erlang/R15B02")
-(setq exec-path (cons "/usr/local/Cellar/erlang/R15B02/bin" exec-path))
-(require 'erlang-start)
-(require 'erlang-flymake)
