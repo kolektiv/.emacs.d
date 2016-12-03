237e707f62986e1a896f399bdae84619023d655a
kolektiv <andrew@xyncro.com>
2016-12-03 13:23:54 +0000

    visuals

--- kolektiv-dark-theme.el
+++ kolektiv-dark-theme.el
@@ -55,58 +55,66 @@
 
    ;; Base
    
-   `(default                            ((,class (:family "Fira Code" :weight light :height 130 :foreground ,gray-9+0 :background ,gray-1+0))))
-   
+   `(default                                 ((,class (:family "Fira Code" :weight light :height 130 :foreground ,gray-9+0 :background ,gray-1+0))))
+ 
    ;; Font Lock
 
-   `(font-lock-builtin-face             ((,class (:foreground "#bbbbbb"))))
-   `(font-lock-comment-face             ((,class (:foreground ,gray-6+0))))
-   `(font-lock-comment-delimiter-face   ((,class (:foreground ,gray-4+0))))
-   `(font-lock-constant-face            ((,class (:foreground "#dddddd"))))
-   `(font-lock-function-name-face       ((,class (:foreground "#eeeeee" :weight normal))))
-   `(font-lock-keyword-face             ((,class (:foreground "#cccccc"))))
-   `(font-lock-string-face              ((,class (:foreground "#aaaaaa" :background ,gray-1+1))))
-   `(font-lock-type-face                ((,class (:foreground "#eeeeee" :weight normal))))
-   `(font-lock-variable-name-face       ((,class (:foreground "#dddddd"))))
+   `(font-lock-builtin-face                  ((,class (:foreground "#bbbbbb"))))
+   `(font-lock-comment-face                  ((,class (:foreground ,gray-6+0))))
+   `(font-lock-comment-delimiter-face        ((,class (:foreground ,gray-4+0))))
+   `(font-lock-constant-face                 ((,class (:foreground "#dddddd"))))
+   `(font-lock-function-name-face            ((,class (:foreground "#eeeeee" :weight normal))))
+   `(font-lock-keyword-face                  ((,class (:foreground "#cccccc"))))
+   `(font-lock-string-face                   ((,class (:foreground "#aaaaaa" :background ,gray-1+1))))
+   `(font-lock-type-face                     ((,class (:foreground "#eeeeee" :weight normal))))
+   `(font-lock-variable-name-face            ((,class (:foreground "#dddddd"))))
 
    ;; Flycheck
 
-   `(flycheck-error                     ((,class (:underline ,error-1))))
-   `(flycheck-info                      ((,class (:underline ,info-1))))
-   `(flycheck-warning                   ((,class (:underline ,warning-1))))
+   `(flycheck-fringe-error                   ((,class (:foreground ,error-1 :background ,error-1))))
+   `(flycheck-fringe-info                    ((,class (:foreground ,info-1 :background ,info-1))))
+   `(flycheck-fringe-warning                 ((,class (:foreground ,warning-1 :background ,warning-1))))
+   `(flycheck-error                          ((,class (:underline ,error-1))))
+   `(flycheck-info                           ((,class (:underline ,info-1))))
+   `(flycheck-warning                        ((,class (:underline ,warning-1))))
 
    ;; Fringe
 
-   `(fringe                             ((,class (:background ,gray-1-1 :foreground ,gray-5+0))))
+   `(fringe                                  ((,class (:background ,gray-1-1 :foreground ,gray-5+0))))
 
+   ;; Minibuffer
+
+   `(minibuffer-prompt                       ((,class (:foreground "#aaaaaa" :box (:line-width 5 :color ,gray-1+0)))))
+   `(minibuffer-noticeable-prompt            ((,class (:inherit minibuffer-prompt :foreground "#eeeeee"))))
+   
    ;; Mode Line
    
-   `(mode-line                          ((,class (:foreground ,gray-7+0 :background ,gray-2+0 :box (:line-width 5 :color ,gray-2+0)))))
-   `(mode-line-buffer-id                ((,class (:foreground "#dddddd" :weight light))))
-   `(mode-line-emphasis                 ((,class (:foreground "#ffffff" :weight normal))))
-   `(mode-line-highlight                ((,class (:background ,gray-3+0 :box (:line-width 5 :color ,gray-3+0)))))
-   `(mode-line-inactive                 ((,class (:inherit mode-line :background ,gray-1+2 :box (:line-width 5 :color ,gray-1+2)))))
+   `(mode-line                               ((,class (:foreground ,gray-7+0 :background ,gray-2+0 :box (:line-width 5 :color ,gray-2+0)))))
+   `(mode-line-buffer-id                     ((,class (:foreground "#dddddd" :weight light))))
+   `(mode-line-emphasis                      ((,class (:foreground "#ffffff" :weight normal))))
+   `(mode-line-highlight                     ((,class (:background ,gray-3+0 :box (:line-width 5 :color ,gray-3+0)))))
+   `(mode-line-inactive                      ((,class (:inherit mode-line :background ,gray-1+2 :box (:line-width 5 :color ,gray-1+2)))))
 
    ;; Rainbow Delimiters
 
-   `(rainbow-delimiters-depth-1-face    ((,class (:foreground ,gray-5+0))))
-   `(rainbow-delimiters-depth-2-face    ((,class (:foreground ,gray-6+0))))
-   `(rainbow-delimiters-depth-3-face    ((,class (:foreground ,gray-7+0))))
-   `(rainbow-delimiters-depth-4-face    ((,class (:foreground ,gray-8+0))))
-   `(rainbow-delimiters-depth-5-face    ((,class (:foreground ,gray-9+0))))
-   `(rainbow-delimiters-depth-6-face    ((,class (:foreground ,gray-9+0))))
-   `(rainbow-delimiters-depth-7-face    ((,class (:foreground ,gray-9+0))))
-   `(rainbow-delimiters-depth-8-face    ((,class (:foreground ,gray-9+0))))
-   `(rainbow-delimiters-mismatched-face ((,class (:foreground ,warning-1))))
-   `(rainbow-delimiters-unmatched-face  ((,class (:foreground ,error-1))))
+   `(rainbow-delimiters-depth-1-face         ((,class (:foreground ,gray-5+0))))
+   `(rainbow-delimiters-depth-2-face         ((,class (:foreground ,gray-6+0))))
+   `(rainbow-delimiters-depth-3-face         ((,class (:foreground ,gray-7+0))))
+   `(rainbow-delimiters-depth-4-face         ((,class (:foreground ,gray-8+0))))
+   `(rainbow-delimiters-depth-5-face         ((,class (:foreground ,gray-9+0))))
+   `(rainbow-delimiters-depth-6-face         ((,class (:foreground ,gray-9+0))))
+   `(rainbow-delimiters-depth-7-face         ((,class (:foreground ,gray-9+0))))
+   `(rainbow-delimiters-depth-8-face         ((,class (:foreground ,gray-9+0))))
+   `(rainbow-delimiters-mismatched-face      ((,class (:foreground ,warning-1))))
+   `(rainbow-delimiters-unmatched-face       ((,class (:foreground ,error-1))))
 
    ;; Version Control
 
-   `(vc-state-base                      ((,class (:foreground ,gray-6+0))))
-   `(vc-conflict-state                  ((,class (:inherit vc-state-base :foreground ,error-1))))
-   `(vc-edited-state                    ((,class (:inherit vc-state-base :foreground ,gray-9+0))))
-   `(vc-locally-added-state             ((,class (:inherit vc-state-base :foreground ,gray-9+0 :slant italic))))
-   `(vc-removed-state                   ((,class (:inherit vc-state-base :foreground ,gray-9+0 :strike-through t))))
+   `(vc-state-base                           ((,class (:foreground ,gray-6+0))))
+   `(vc-conflict-state                       ((,class (:inherit vc-state-base :foreground ,error-1))))
+   `(vc-edited-state                         ((,class (:inherit vc-state-base :foreground ,gray-9+0))))
+   `(vc-locally-added-state                  ((,class (:inherit vc-state-base :foreground ,gray-9+0 :slant italic))))
+   `(vc-removed-state                        ((,class (:inherit vc-state-base :foreground ,gray-9+0 :strike-through t))))
 
    ))
    
