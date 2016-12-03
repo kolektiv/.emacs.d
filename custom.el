53f97e7db56a0a13a7dd0f0f339acd835284bd4a
kolektiv <andrew@xyncro.com>
2016-12-03 13:23:54 +0000

    visuals

--- custom.el
+++ custom.el
@@ -6,12 +6,3 @@
  '(package-selected-packages
    (quote
 	(exec-path-from-shell flycheck use-package smart-mode-line rainbow-delimiters))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(vc-conflict-state ((t (:inherit vc-state-base :foreground "#ff0000"))))
- '(vc-edited-state ((t (:inherit vc-state-base :foreground "#999999"))))
- '(vc-locally-added-state ((t (:inherit vc-state-base :foreground "#999999" :slant italic))))
- '(vc-removed-state ((t (:inherit vc-state-base :foreground "#999999" :strike-through t)))))
